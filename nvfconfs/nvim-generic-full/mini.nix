de0cef58bcbde7d331183d7bbb8439dd0297a7d6
ppenguin <hieronymusv@gmail.com>
2026-01-18 15:53:26 +0100

    update mini.operators keymap

--- nvfconfs/nvim-generic-full/mini.nix
+++ nvfconfs/nvim-generic-full/mini.nix
@@ -1,6 +1,7 @@
 {lib, ...}: let
   inherit (lib.nvim.binds) mkKeymap;
-  mkKeymapD = mode: key: cmd: desc: mkKeymap mode key cmd {inherit desc;};
+  mkKeymapD = mode: key: cmd: desc:
+    mkKeymap mode key cmd {inherit desc;};
 in {
   config.vim = {
     mini = {
@@ -22,5 +23,12 @@ in {
       (mkKeymapD "n" "<leader>imt" "<CMD>lua MiniMap.toggle()<CR>" "mini-map toggle")
       (mkKeymapD "n" "<leader>ims" "<CMD>lua MiniMap.toggle_side()<CR>" "mini-map toggle side (left)")
     ];
+    # NOTE: raw lua, because nvf escapes "+gr to \"+gr which doesn't work
+    luaConfigRC.mini-operators-clipboard = lib.nvim.dag.entryAfter ["mini-operators"] ''
+      vim.keymap.set({'n', 'x'}, '<leader>gr', '"+gr', {
+        remap = true,
+        desc = 'mini.operator replace from system clipboard'
+      })
+    '';
   };
 }
