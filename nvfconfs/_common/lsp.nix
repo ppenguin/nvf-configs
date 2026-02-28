631cd39648715332d4e7c85935db585c1704180d
ppenguin <hieronymusv@gmail.com>
2026-02-28 14:46:37 +0100

    add filetree copy hack; minor changes

--- nvfconfs/_common/lsp.nix
+++ nvfconfs/_common/lsp.nix
@@ -65,7 +65,8 @@ in {
           pattern = "sh",
           callback = function()
             vim.bo.shiftwidth = 4
-            vim.bo.softtabstop = 4
+            vim.bo.tabstop = 4
+            vim.bo.softtabstop = 0
             vim.bo.expandtab = false
           end,
         })
